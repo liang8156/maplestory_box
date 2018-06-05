43e464822d2d90ea20d8672d492c1519821626ea
alexLiang <liang8156@gmail.com>
2018-06-05 22:56:00 +0800

    可記錄選擇潛能 但仍無法只選擇3樣和取消

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,19 @@ function run(index) {
         case '使用': console.warn(index)
             let choicePotential = [];
             for (let i = 0; i < 6; i++) {
-                choicePotential[i] = armsPotentialDataBasis[getRandom(6)];
+                choicePotential[i] = armsPotentialDataBasis[getRandom(armsPotentialDataBasis.length)];
             }
-            console.warn(choicePotential)
+            document.getElementById("potentialBox").innerHTML = ""
+            potential = []
+            for (let i = 0; i < choicePotential.length; i++) {
+                var calelem = document.createElement("div");
+                // calelem.setAttribute("class", "button" + buttonElement[i]);
+                calelem.setAttribute('onclick', "choice6to3(this)");
+                calelem.setAttribute('id', choicePotential[i]);
+                calelem.innerHTML = choicePotential[i];
+                document.getElementById("potentialBox").appendChild(calelem);
+            }
+
             break;
         case '取消': console.warn(index);
             break;
@@ -16,13 +26,27 @@ function run(index) {
 function getRandom(x) {
     return Math.floor(Math.random() * x);
 };
+function choice6to3(test) {
+    console.warn(test.id)
+    console.warn("choice6to3")
+    console.log(potential.indexOf(test.id));
+    potential = potential.concat(test.id)
+    console.warn("potential", potential)
+}
 ////////////////////////鍵盤控制    
 let potential = ["攻擊BOSS怪物時傷害-30%", "攻擊BOSS怪物時傷害-20%", "攻擊BOSS怪物時傷害-40%"]
 let armsPotentialDataBasis = ["STR:+12%", "STR:+9%", "DEX:+12%", "DEX:+9%",
-    "LUK:+12%", "LUK:+9%"]
+    "LUK:+12%", "LUK:+9%", "攻擊BOSS怪物時傷害+40%", "攻擊BOSS怪物時傷害+35%", "攻擊BOSS怪物時傷害+30%",
+    "攻擊BOSS怪物時傷害+20%"]
 
-var newContent = document.createTextNode(potential);
-document.getElementById("potential").appendChild(newContent);
+
+for (let i = 0; i < potential.length; i++) {
+    var calelem = document.createElement("div");
+    // calelem.setAttribute("class", "button" + buttonElement[i]);
+    //calelem.setAttribute('onclick', "run('" + buttonElement[i] + "')");
+    calelem.innerHTML = potential[i];
+    document.getElementById("potentialBox").appendChild(calelem);
+}
 
 let buttonElement = ["使用", "取消", "確認"];
 for (let i = 0; i < buttonElement.length; i++) {
